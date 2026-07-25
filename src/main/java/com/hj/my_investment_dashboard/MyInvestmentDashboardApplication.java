c77f3d41fe76208a2b747a923419e1f873ad03d6
unfl1 <flxmaut10@gmail.com>
2026-07-25 12:45:12 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/hj/my_investment_dashboard/MyInvestmentDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.hj.my_investment_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInvestmentDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyInvestmentDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hj/my_investment_dashboard/MyInvestmentDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hj.my_investment_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInvestmentDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
