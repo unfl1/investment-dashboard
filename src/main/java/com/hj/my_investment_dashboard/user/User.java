9715c4619bd85c3722deca9736e22d4d2828d439
unfl1 <flxmaut10@gmail.com>
2026-07-26 21:30:49 +0900

    feat: 로그인 및 회원가입용 User 엔티티 추가

---
+++ src/main/java/com/hj/my_investment_dashboard/user/User.java
@@ -0,0 +1,26 @@
+package com.hj.my_investment_dashboard.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    private String userId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
