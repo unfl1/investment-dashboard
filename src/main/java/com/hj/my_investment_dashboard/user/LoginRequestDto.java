1c0df005f18b2def54fb82c045eecb5c5d35af87
unfl1 <flxmaut10@gmail.com>
2026-08-03 21:35:36 +0900

    refactor: Dto에 lombok 적용

--- src/main/java/com/hj/my_investment_dashboard/user/LoginRequestDto.java
+++ src/main/java/com/hj/my_investment_dashboard/user/LoginRequestDto.java
@@ -1,23 +1,11 @@
 package com.hj.my_investment_dashboard.user;
 
+import lombok.Data;
+
+@Data
 public class LoginRequestDto {
 
     private String userId;
     private String password;
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/hj/my_investment_dashboard/user/SignupRequestDto.java
+++ src/main/java/com/hj/my_investment_dashboard/user/SignupRequestDto.java
@@ -1,32 +1,12 @@
 package com.hj.my_investment_dashboard.user;
 
+import lombok.Data;
+
+@Data
 public class SignupRequestDto {
 
     private String userId;
     private String email;
     private String password;
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
